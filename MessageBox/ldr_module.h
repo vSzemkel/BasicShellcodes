41c56a5c8922e3514baa8238e489eb09eb27eef5
Marcin Buchwald <marcin.buchwald@agora.pl>
2017-02-02 16:30:50 +0100

    LDR_MODULE

---
+++ MessageBox/ldr_module.h
@@ -0,0 +1,15 @@
+#pragma once
+
+#include <Windows.h>
+#include <Subauth.h>
+
+// Perspective of InInitilizationOrder modules list
+struct LDR_MODULE
+{
+	LIST_ENTRY InInitOrder;        // 0x00
+	DWORD DllBase;                 // 0x08
+	DWORD EntryPoint;              // 0x0C
+	DWORD Reserved;                // 0x14
+	UNICODE_STRING FullDllName;    // 0x24
+	UNICODE_STRING BaseDllName;    // 0x2C
+};
